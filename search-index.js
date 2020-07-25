4a9560ba7d7b3ede1ebbc32fcadacc979fe3a37a
SirWindfield <SirWindfield@users.noreply.github.com>
2020-07-25 10:28:49 +0000

    deploy: 3c58d9729e5492bae7ec09faf2dadfd76e0d1d45

---
+++ search-index.js
@@ -0,0 +1,4 @@
+var searchIndex = JSON.parse('{\
+"conventional_commits_parser":{"doc":"","i":[],"p":[]}\
+}');
+addSearchOptions(searchIndex);initSearch(searchIndex);
---
+++ source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["conventional_commits_parser"] = {"name":"","files":["lib.rs"]};
+createSourceSidebar();
